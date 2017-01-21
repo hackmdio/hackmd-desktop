a25de1f8e14409ca4effe578e3dc65487be74a2c
Yukai Huang <yukaihuangtw@gmail.com>
2017-01-21 21:32:58 +0800

    Implement inline titlebar

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 const os = require('os');
 const fs = require('fs');
+const path = require('path');
 
 let mainWin;
 
 const winOption = {
 	width: 1024,
-	height: 768,
-	webPreferences: {
-		webSecurity: false,
-		nodeIntegration: false
-	}
+	height: 768
 }
 
 const isDarwin = os.platform() === 'darwin';
@@ -18,25 +15,11 @@ const isDarwin = os.platform() === 'darwin';
 function initializeApp () {
 	mainWin = new BrowserWindow(
 		(isDarwin
-			? Object.assign({}, winOption, {titleBarStyle: 'hidden-inset'})
+			? Object.assign({}, winOption, {titleBarStyle: 'hidden'})
 			: winOption)
 	);
 
-	if (process.env.NODE_ENV === 'development') {
-		mainWin.loadURL('http://localhost:3000');
-	} else {
-		mainWin.loadURL('https://hackmd.io');
-	}
-
-	mainWin.webContents.on('did-finish-load', function() {
-		let cssPath = isDarwin ? '/static/darwin.css' : '/static/app.css';
-
-		fs.readFile(__dirname + cssPath, 'utf-8', function(error, data) {
-			if (!error) {
-				mainWin.webContents.insertCSS(data);
-			}
-		});
-	});
+	mainWin.loadURL(`file://${path.join(__dirname, 'index.html')}`);
 }
 
 app.on('ready', () => {
---
+++ renderer.js
@@ -0,0 +1,75 @@
+const fs = require('electron').remote.require('fs');
+const os = require('electron').remote.require('os');
+const path = require('electron').remote.require('path');
+const {BrowserWindow} = require('electron').remote;
+const {clipboard} = require('electron');
+
+const SERVER_URL = 'https://hackmd.io';
+
+const isDarwin = os.platform() === 'darwin';
+
+const winOption = {
+	width: 1024,
+	height: 768
+}
+
+onload = () => {
+	if (window.location.search !== '') {
+		targetURL = window.location.search.match(/\?target=([^?]+)/)[1];
+	} else {
+		targetURL = SERVER_URL;
+	}
+
+	document.body.innerHTML += `<webview src="${targetURL}" id="main-window" disablewebsecurity autosize="on" allowpopups allowfileaccessfromfiles></webview>`;
+
+	const webview = document.getElementById("main-window");
+
+	webview.addEventListener('dom-ready', function(){
+		// set webview title
+		document.querySelector('#navbar-container .title').innerHTML = webview.getTitle();
+
+		// set dark theme if in home page
+		if (webview.getURL().split('?')[0].split('#')[0].match(/https:\/\/hackmd.io\/$/)) {
+			document.querySelector('navbar').className = 'dark';
+		} else {
+			document.querySelector('navbar').className = '';
+		}
+
+		/* bind control buttons event */
+		document.querySelector('#navbar-container .navigate-back').onclick = () => {
+			if (webview.canGoBack()) {
+				webview.goBack();
+			}
+		};
+
+		document.querySelector('#navbar-container .navigate-foward').onclick = () => {
+			if (webview.canGoForward()) {
+				webview.goForward();
+			}
+		};
+
+		document.querySelector('#navbar-container .home').onclick = () => {
+			webview.loadURL(SERVER_URL);
+		}
+
+		document.querySelector('#navbar-container .refresh').onclick = () => {
+			webview.loadURL(webview.getURL());
+		}
+
+		document.querySelector('#navbar-container .title').onclick = () => {
+			clipboard.writeText(webview.getURL());
+			new Notification('URL copied', { title: 'URL copied', body: webview.getURL() });
+		}
+
+		// webview.openDevTools();
+	});
+
+	/* handle _target=blank pages */
+	webview.addEventListener('new-window', (event) => {
+		new BrowserWindow(
+			(isDarwin
+				? Object.assign({}, winOption, {titleBarStyle: 'hidden'})
+				: winOption)
+		).loadURL(`file://${path.join(__dirname, `index.html?target=${event.url}`)}`);
+	});
+}
